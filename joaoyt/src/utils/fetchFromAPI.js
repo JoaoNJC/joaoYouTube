5ba5e0f7ceffc1dae9208da3bece501a564a5ad2
Joao Camba <joaocamba@tests-iMac.local>
2023-06-09 18:19:40 +0100

    Final touches

--- joaoyt/src/utils/fetchFromAPI.js
+++ joaoyt/src/utils/fetchFromAPI.js
@@ -5,10 +5,10 @@ const BASE_URL = 'https://youtube-v31.p.rapidapi.com/captions';
 const options = {
   url: BASE_URL,
   params: {
-    maxResults: '50'
+    maxResults: 50,
   },
   headers: {
-    'X-RapidAPI-Key': ProcessingInstruction.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
   }
 };
